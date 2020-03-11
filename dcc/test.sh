190efee38b879079b1eb94877a7193c26e698a68
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-03-11 15:16:55 +0900

    fix test statements

--- dcc/test.sh
+++ dcc/test.sh
@@ -16,32 +16,30 @@ try() {
   fi
 }
 
-# until step5
-try 0 0
-try 42 42
-try 21 '5+20-4'
-try 41 ' 12 + 34 - 5 '
-try 47 '5+6*7'
-try 15 '5*(9-6)'
-try 4 '(3+5)/2'
+echo "until step6: plus, minus, cal, div, ()"
+try 0 "0;"
+try 42 "42"
+try 21 "5+20-4;"
+try 41 " 12 + 34 - 5 ;"
+try 47 "5+6*7;"
+try 15 "5*(9-6);"
+try 4 "(3+5)/2;"
+try 10 "-10+20;"
 
-# step6
-try 10 '-10+20'
-
-# step7: relational
-try 0 "0==1"
-try 1 "0==0"
-try 0 "1!=1"
-try 1 "1!=88"
-try 0 "1<0"
-try 1 "0<1"
-try 0 "13<=12"
-try 1 "13<=14"
-try 1 "7<=7"
-try 0 "0>1"
-try 1 "1>0"
-try 0 "0>=1"
-try 1 "2>=1"
-try 1 "1>=1"
+echo "step7: relational"
+try 0 "0==1;"
+try 1 "0==0;"
+try 0 "1!=1;"
+try 1 "1!=88;"
+try 0 "1<0;"
+try 1 "0<1;"
+try 0 "13<=12;"
+try 1 "13<=14;"
+try 1 "7<=7;"
+try 0 "0>1;"
+try 1 "1>0;"
+try 0 "0>=1;"
+try 1 "2>=1;"
+try 1 "1>=1;"
 
 echo OK
