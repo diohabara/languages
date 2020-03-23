958f53be9d8cb425f6f2cf67db0e6236a85acf3d
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-03-23 16:36:32 +0900

    add functions for vector

--- dcc/container.c
+++ dcc/container.c
@@ -31,3 +31,25 @@ bool is_alnum(char c) {
   return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') ||
          ('0' <= c && c <= '9') || (c == '_');
 }
+
+Vector* new_vector(void) {
+  Vector* vec = (Vector*)malloc(sizeof(Vector));
+  vec->data = (void**)malloc(sizeof(void*) * 16);
+  vec->capacity = 16;
+  vec->len = 0;
+  return vec;
+}
+
+void vec_push(Vector* vec, void* ele) {
+  if (vec->capacity == vec->len) {
+    vec->capacity *= 2;
+    vec->data = realloc(vec->data, sizeof(void*) * vec->capacity);
+  }
+  vec->data[vec->len++] = ele;
+}
+
+void* vec_at(Vector* vec, int index) {
+  if (index < 0 || vec->len <= index)
+    error("The size of vector is %d, but the index is %d", vec->len, index);
+  return vec->data[index];
+}
